2d8b6be11bee9ed1857f8831aa95fc4cc87a1a6c
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-21 14:06:26 +0900

    hot fix

--- theme/basic/index.php
+++ theme/basic/index.php
@@ -1,6 +1,8 @@
 <?php
 if (!defined('_INDEX_')) define('_INDEX_', true);
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+if (!$member['mb_id'])
+	header('Location: /bbs/login.php');
 
 if (G5_IS_MOBILE) {
     include_once(G5_THEME_MOBILE_PATH.'/index.php');
